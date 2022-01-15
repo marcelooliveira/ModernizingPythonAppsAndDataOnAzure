c77d41d33a0d61cb017fed4a83467c309dddab7e
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-01-15 12:34:29 -0300

    PostgreSQL configuration

--- article1/conduit/settings.py
+++ article1/conduit/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'conduit_db',
+        'USER': 'conduit_user',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -132,6 +135,7 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
     'localhost:4000',
+    'localhost:8080'
 )
 
 # Tell Django about the custom `User` model we created. The string
