06f5709b241d97f9d7605474abc1420beebc58d7
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-06 19:37:45 -0300

    PyJWT and six work on azure

--- django-conduit-master/conduit/views.py
+++ django-conduit-master/conduit/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! FROM PYTHON HELLO WORLD 7:13")
+    return HttpResponse("Hello, world! FROM PYTHON HELLO WORLD 7:34")
