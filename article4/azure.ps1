50188f215cd64fb510236ab93b08c9f9cc3d6639
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-03-06 23:59:23 -0300

    .

--- article4/azure.ps1
+++ article4/azure.ps1
@@ -2,23 +2,23 @@
 
 # BUILD IMAGE conduit:v1
 
-# az login
+# az group create --name conduit-rg --location eastus
 
-az group create --name conduit-rg --location eastus
+# az aks create --resource-group conduit-rg --name conduit-cluster --node-count 1 --generate-ssh-keys
 
-az aks create --resource-group conduit-rg --name conduit-cluster --node-count 1 --generate-ssh-keys
+az login
 
 az aks get-credentials --resource-group conduit-rg --name conduit-cluster
 
 az ad sp create-for-rbac --role AcrPull
 
-az acr login --name conduit-acr
+az acr login --name conduitacr
 
-az aks update -n conduit-cluster -g conduit-rg --attach-acr conduit-acr
+az aks update -n conduit-cluster -g conduit-rg --attach-acr conduitacr
 
-docker tag conduit:v1 conduit-acr.azurecr.io/conduit:v1
+docker tag conduit:v1 conduitacr.azurecr.io/conduit:v1
 
-docker push conduit-acr.azurecr.io/conduit:v1
+docker push conduitacr.azurecr.io/conduit:v1
 
 kubectl apply -f conduit-app.yaml
 
